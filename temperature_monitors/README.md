796e1389572420afa77c83412bfa9de011ce0988
caleb <caleb.yourison@gmail.com>
2026-02-07 05:25:39 -0700

    temperature monitors

---
+++ temperature_monitors/README.md
@@ -0,0 +1,34 @@
+# Temperature Sensors
+ ---
+
+Measure temperature (C or F) from one or more sensors (DS18B20) and print the output.
+This can be used in conjunction with other peripherals or methods such as an attached screen or relay device to trigger a local device.
+
+Please reference the diagram.
+
+(Image Credit: [raspberrypi.com](https://www.raspberrypi.com/documentation/microcontrollers/pico-series.html#pico-1-family))
+
+![My plot](diagram.jpg)
+
+This project assumes each device has a dedicated pin.  
+Recommended resistor values of 2.2K to 4.7K depending on wire length.  
+Twisted pair CAT5e or similar recommended.
+
+Modify values as needed:
+- pin_numbers are mapped to their corresponding sensor names.
+- measurement period length, sample frequency seconds (additive, default values produce readings every ~3 seconds)
+- Celsius by default, convert to Fahrenheit if desired
+
+```bash'
+pins = {
+    15: "First Sensor Name",
+    16: "Second Sensor Name",
+    17: "Third Sensor Name",
+}
+
+measurement_period_ms = 750
+sample_frequency_seconds = 2
+
+fahrenheit: bool = False
+```
+
---
+++ temperature_monitors/main.py
@@ -0,0 +1,53 @@
+from machine import Pin
+import onewire, ds18x20, time
+
+pins = {
+    15: "First",
+   16: "Second",
+   17: "Third",
+}
+
+measurement_period_ms = 750
+sample_frequency_seconds = 2
+
+fahrenheit: bool = False
+
+sensors = {}
+
+# Initialize each sensor on its own pin
+for pin_num, name in pins.items():
+    pin = Pin(pin_num)
+    ow = onewire.OneWire(pin)
+    ds = ds18x20.DS18X20(ow)
+
+    roms = ds.scan()
+    if not roms:
+        raise RuntimeError(f"No sensor found on GPIO {pin_num}")
+
+    # Use the first rom since only one sensor is connected per pin
+    sensors[name] = {
+        "ds": ds,
+        "rom": roms[0]
+    }
+
+print("Initialized sensors:", sensors.keys())
+
+while True:
+    temps = {}
+
+    # Conversions
+    for s in sensors.values():
+        s["ds"].convert_temp()
+
+    time.sleep_ms(measurement_period_ms)
+
+    # Read temperatures, overwrite dict key with latest value
+    for name, s in sensors.items():
+        temp = s["ds"].read_temp(s["rom"])
+        if fahrenheit:
+            temp = temp * (9 / 5) + 32
+        temps[name] = temp
+
+    print(temps)
+    time.sleep(sample_frequency_seconds)
+
